d43027c30d8bd3038b9833c8754cf44f109da823
aggre <hiroyuki.aggre@gmail.com>
2018-11-01 10:59:30 +0900

    init

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript'
+import postcss from 'rollup-plugin-transform-postcss'
+import postcssPresetEnv from 'postcss-preset-env'
+
+export default {
+	input: './src/index.ts',
+	plugins: [
+		typescript(),
+		postcss({
+			plugins: [postcssPresetEnv]
+		})
+	],
+	output: {
+		file: './src/index.js',
+		format: 'umd'
+	}
+}
