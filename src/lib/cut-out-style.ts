35eda4f472f6ff2422293edce6dc9fd31d82ae3a
aggre <hiroyuki.aggre@gmail.com>
2018-11-20 18:38:41 +0900

    bug fix: regular expression for cutting out of CSS

--- src/lib/cut-out-style.ts
+++ src/lib/cut-out-style.ts
@@ -3,7 +3,7 @@ const reg = /(<style((?!>).)*>((?!<\/style>)[\s\S])*<\/style>)/g
 export const cutOutStyle = (html: string) => ({
 	styles: (matches =>
 		matches
-			? matches.map(i => i.replace(/<\/?style((?!>).)*>/, '')).join('')
+			? matches.map(i => i.replace(/<\/?style((?!>).)*>/g, '')).join('')
 			: '')(html.match(reg)),
 	content: html.replace(reg, '')
 })
