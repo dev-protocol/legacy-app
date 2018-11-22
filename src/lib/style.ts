14ca3e5193291bcb45e308b05d93fe692d2c900d
aggre <hiroyuki.aggre@gmail.com>
2018-11-22 12:25:36 +0900

    enable autoprefixer

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -3,7 +3,13 @@ import * as preset from 'postcss-preset-env'
 import { html } from './html'
 
 export const style = createStyle({
-	plugins: [preset({ stage: 0 })],
+	plugins: [
+		preset({
+			stage: 0,
+			browsers: 'last 2 versions',
+			autoprefixer: { grid: true }
+		})
+	],
 	// tslint:disable-next-line:typedef
 	build(css) {
 		return html`<style>${css}</style>`
