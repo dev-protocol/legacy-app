f51c83013bc35106564d22eef6b1e6e33d05b6fb
aggre <hiroyuki.aggre@gmail.com>
2018-12-04 21:04:55 +0900

    Add favicons

--- src/page/error.ts
+++ src/page/error.ts
@@ -24,6 +24,14 @@ export const error = async (
 		<link rel="canonical" href="${proto}://${config.domain}${request.url}">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:400,700" rel="stylesheet">
 		<title>${message} - Dev | Token for OSS sustainability</title>
+		<link rel="icon" href="//asset.devtoken.rocks/favicon.ico">
+		<link rel="icon" type="image/png" sizes="32x32" href="//asset.devtoken.rocks/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="//asset.devtoken.rocks/favicon-16x16.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="//asset.devtoken.rocks/apple-touch-icon.png">
+		<link rel="manifest" href="//asset.devtoken.rocks/site.webmanifest">
+		<link rel="mask-icon" href="//asset.devtoken.rocks/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#000000">
+		<meta name="theme-color" content="#000000">
 		${await style`
 			body {
 				background: black;
