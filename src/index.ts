7839f2e57cace94599d7b4e0189109a4c8cd80f6
aggre <hiroyuki.aggre@gmail.com>
2018-12-04 12:36:17 +0900

    Supports Now 2.0

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import { send } from 'micro'
-import { IncomingMessage, ServerResponse } from 'http'
-import { parse } from 'url'
-import { error } from './page/error'
-import { packageR } from './route/package-r'
-import { badge } from './route/badge'
+import { app } from './app'
 
-export default async (request: IncomingMessage, res: ServerResponse) => {
-	const { url = '' } = request
-	const parsed = parse(url)
-	const { pathname = '' } = parsed
-	const [, route] = pathname.split('/')
-	const body =
-		route === 'package'
-			? await packageR(pathname, request)
-			: route === 'badge'
-				? await badge(pathname, res)
-				: false
-	const status = body ? 200 : 404
-	return send(res, status, body || (await error({ status, request })))
-}
+export = app
