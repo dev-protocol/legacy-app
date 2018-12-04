0958640b12e55381bf0bc205ede25bb7c48a45f9
aggre <hiroyuki.aggre@gmail.com>
2018-12-04 19:42:51 +0900

    Changed lit-ntml to native implementation because it cannot be running on Now

--- src/lib/amp.ts
+++ src/lib/amp.ts
@@ -1,7 +1,7 @@
-import ntml from 'lit-ntml'
 import { createStyle } from 'lit-style'
 import { cutOutStyle } from './cut-out-style'
 import * as cssnano from 'cssnano'
+import { html } from './html'
 
 export const style = createStyle({
 	plugins: [cssnano()],
@@ -10,30 +10,19 @@ export const style = createStyle({
 
 const boilerplate =
 	'<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>'
-const beforeHead = (html: string, content: string) =>
-	html.replace(/(<\/head>)/, `${content}$1`)
+const beforeHead = (htm: string, content: string) =>
+	htm.replace(/(<\/head>)/, `${content}$1`)
 
-export const amp = new Proxy(
-	ntml({
-		options: {
-			minify: true,
-			parse: 'html'
-		}
-	}),
-	{
-		apply: async (target, thisArg, args) => {
-			const doc = await target.apply(thisArg, args)
-			const { styles, content } = cutOutStyle(doc)
-			const addedBoilerplate = beforeHead(content, boilerplate)
-			const addedAmpScript = beforeHead(
-				addedBoilerplate,
-				'<script async src="https://cdn.ampproject.org/v0.js"></script>'
-			)
-			const ampCustom = await style`${styles}`
-			return beforeHead(
-				addedAmpScript,
-				`<style amp-custom>${ampCustom}</style>`
-			)
-		}
+export const amp = new Proxy(html, {
+	apply: async (target, thisArg, args) => {
+		const doc = await target.apply(thisArg, args)
+		const { styles, content } = cutOutStyle(doc)
+		const addedBoilerplate = beforeHead(content, boilerplate)
+		const addedAmpScript = beforeHead(
+			addedBoilerplate,
+			'<script async src="https://cdn.ampproject.org/v0.js"></script>'
+		)
+		const ampCustom = await style`${styles}`
+		return beforeHead(addedAmpScript, `<style amp-custom>${ampCustom}</style>`)
 	}
-)
+})
--- src/lib/html.ts
+++ src/lib/html.ts
@@ -1,3 +1,18 @@
-import ntml from 'lit-ntml'
-
-export const html = ntml()
+export const html = async (strings: TemplateStringsArray, ...values) => {
+	const resolved = await Promise.all<string>(
+		values.map(
+			async value =>
+				typeof value === 'function' || value instanceof Function
+					? value()
+					: value
+		)
+	)
+	const resolvedLength = resolved.length
+	const uni = await Promise.all(
+		strings.map(async (n, i) => {
+			const dn = resolved[i]
+			return i >= resolvedLength ? n : `${n}${dn}`
+		})
+	)
+	return uni.join('')
+}
