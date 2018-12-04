dfd0e3e09c03d45a94d1a18ee3a771d864049883
aggre <hiroyuki.aggre@gmail.com>
2018-12-04 13:39:49 +0900

    Remove "badge" from routes

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,13 @@ import { IncomingMessage, ServerResponse } from 'http'
 import { parse } from 'url'
 import { error } from './page/error'
 import { packageR } from './route/package-r'
-import { badge } from './route/badge'
 
 export const app = async (request: IncomingMessage, res: ServerResponse) => {
 	const { url = '' } = request
 	const parsed = parse(url)
 	const { pathname = '' } = parsed
 	const [, route] = pathname.split('/')
-	const body =
-		route === 'package'
-			? await packageR(pathname, request)
-			: route === 'badge'
-				? await badge(pathname, res)
-				: false
+	const body = route === 'package' ? await packageR(pathname, request) : false
 	const status = body ? 200 : 404
 	return send(res, status, body || (await error({ status, request })))
 }
