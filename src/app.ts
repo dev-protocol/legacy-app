7badc3a6c920d85e9b5736bd0cf7ebea5af238a9
aggre <hiroyuki.aggre@gmail.com>
2018-12-15 12:01:14 +0900

    add endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,27 @@ import { parse } from 'url'
 import { error } from './page/error'
 import { packageR } from './route/package-r'
 import { doc } from './route/doc'
+import { api } from './route/api'
 
 export const app = async (request: IncomingMessage, res: ServerResponse) => {
 	const { url = '' } = request
 	const parsed = parse(url)
 	const { pathname = '' } = parsed
 	const [, route] = pathname.split('/')
-	const body =
+	const result =
 		route === 'package'
 			? await packageR(pathname, request)
 			: route === 'doc'
 			? await doc(pathname, request)
+			: route === 'api'
+			? await api(pathname, request)
 			: false
-	const status = body ? 200 : 404
-	return send(res, status, body || (await error({ status, request })))
+	const status = result instanceof Error ? 400 : result ? 200 : 404
+	const body =
+		result instanceof Error
+			? { message: result.message }
+			: result
+			? result
+			: await error({ status, request })
+	return send(res, status, body)
 }
