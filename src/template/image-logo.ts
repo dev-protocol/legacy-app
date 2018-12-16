58cae29e30f0e9e8ad29c050e38023527e521e47
aggre <hiroyuki.aggre@gmail.com>
2018-12-16 12:09:14 +0900

    use asset of "asset.devtoken.rocks"

--- src/template/image-logo.ts
+++ src/template/image-logo.ts
@@ -2,9 +2,9 @@ import { html } from '../lib/html'
 
 export const imageLogo = async () => html`
 	<amp-img alt='Dev'
-		src=//uploads-ssl.webflow.com/5b4dad82e11a7877ac027d2a/5bf6781a4ae342ebc280c82e_dev__logo--white.svg
-		width=712.02
-		height=381.05
+		src=//asset.devtoken.rocks/logo--white.svg
+		width=408
+		height=203
 		layout=responsive>
 	</amp-img>
 `
