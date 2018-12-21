c0f7882a135d1a96a7d9b88870f29c89fa46bf78
aggre <hiroyuki.aggre@gmail.com>
2018-12-21 16:30:14 +0900

    Change brand logo

--- src/template/image-logo.ts
+++ src/template/image-logo.ts
@@ -4,7 +4,7 @@ export const imageLogo = async () => html`
 	<amp-img alt='Dev'
 		src=//asset.devtoken.rocks/logo--white.svg
 		width=408
-		height=203
+		height=215
 		layout=responsive>
 	</amp-img>
 `
