7a778752af24df791757bc9e4101f3f2d1b26774
aggre <hiroyuki.aggre@gmail.com>
2019-01-06 12:12:37 +0900

    add page: start

--- src/route/doc.ts
+++ src/route/doc.ts
@@ -1,9 +1,12 @@
 import { IncomingMessage } from 'http'
 import { howToUseMarkdownBadge } from '../page/doc/how-to-use-markdown-badge'
+import { start } from '../page/doc/start'
 
 export const doc = async (pathname: string, request: IncomingMessage) => {
 	const [, , name] = pathname.split('/')
 	return name === 'how-to-use-markdown-badge'
 		? howToUseMarkdownBadge({ request })
+		: name === 'start'
+		? start({ request })
 		: false
 }
