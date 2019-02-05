e184623ee427f6e6cfb75a2ea3cf22c83a14ed91
aggre <hiroyuki.aggre@gmail.com>
2019-02-05 11:43:19 +0900

    fix typo

--- src/template/chart.ts
+++ src/template/chart.ts
@@ -2,7 +2,7 @@ import { ampImage } from './amp/amp-image'
 
 export const chart = () =>
 	ampImage({
-		alt: 'Downloas chart',
+		alt: 'Downloads chart',
 		src: '//asset.devtoken.rocks/lp/chart/2019-01.png',
 		width: 2632,
 		height: 1400,
