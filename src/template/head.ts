b684176a8bf803383996ddbb064282a8ccc0a37f
aggre <hiroyuki.aggre@gmail.com>
2019-09-11 18:00:05 +0900

    fix bug: unexpected url

--- src/template/head.ts
+++ src/template/head.ts
@@ -17,8 +17,6 @@ interface Opts {
 }
 
 const defaultTitle = 'Dev - Tokens for OSS sustainability'
-const absolutePath = ({ protocol = 'https', host, path = '' }: URL) =>
-	`${protocol}://${host}${path}`
 
 export const head = async ({
 	title,
@@ -33,7 +31,7 @@ export const head = async ({
 			name="viewport"
 			content="width=device-width,minimum-scale=1,initial-scale=1"
 		/>
-		<link rel="canonical" href="${absolutePath(url)}" />
+		<link rel="canonical" href="${url}" />
 		<link
 			href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:400,700"
 			rel="stylesheet"
@@ -41,57 +39,39 @@ export const head = async ({
 		<title>${escapeHTML(`${title ? `${title} - ` : ''}${defaultTitle}`)}</title>
 		<link
 			rel="icon"
-			href="${absolutePath({
-				host: '/public',
-				path: '/favicon.ico'
-			})}"
+			href="/public/favicon.ico"
 		/>
 		<link
 			rel="icon"
 			type="image/png"
 			sizes="32x32"
-			href="${absolutePath({
-				host: '/public',
-				path: '/favicon-32x32.png'
-			})}"
+			href="/public/favicon-32x32.png"
 		/>
 		<link
 			rel="icon"
 			type="image/png"
 			sizes="16x16"
-			href="${absolutePath({
-				host: '/public',
-				path: '/favicon-16x16.png'
-			})}"
+			href="/public/favicon-16x16.png"
 		/>
 		<link
 			rel="apple-touch-icon"
 			sizes="180x180"
-			href="${absolutePath({
-				host: '/public',
-				path: '/apple-touch-icon.png'
-			})}"
+			href="/public/apple-touch-icon.png"
 		/>
 		<link
 			rel="manifest"
-			href="${absolutePath({
-				host: '/public',
-				path: '/site.webmanifest'
-			})}"
+			href="/public/site.webmanifest"
 		/>
 		<link
 			rel="mask-icon"
-			href="${absolutePath({
-				host: '/public',
-				path: '/safari-pinned-tab.svg'
-			})}"
+			href="/public/safari-pinned-tab.svg"
 			color="#000000"
 		/>
 		<meta name="msapplication-TileColor" content="#000000" />
 		<meta name="theme-color" content="#000000" />
 		<meta property="og:site_name" content="Dev" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="${absolutePath(url)}" />
+		<meta property="og:url" content="${url}" />
 		<meta property="og:title" content="${escapeHTML(title || defaultTitle)}" />
 		<meta name="description" content="${escapeHTML(description)}" />
 		<meta property="og:description" content="${escapeHTML(description)}" />
@@ -99,17 +79,11 @@ export const head = async ({
 		<meta name="twitter:description" content="${escapeHTML(description)}" />
 		<meta
 			property="og:image"
-			content="${absolutePath({
-				host: '/public',
-				path: image ? image : '/og.png'
-			})}"
+			content="${image ? image : '/public/og.png'}"
 		/>
 		<meta
 			name="twitter:image"
-			content="${absolutePath({
-				host: '/public',
-				path: image ? image : '/og.png'
-			})}"
+			content="${image ? image : '/public/og.png'}"
 		/>
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:site" content="@devtoken_rocks" />
